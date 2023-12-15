ca764cca438671dcc4fe89ace7057934b0c9a157
Nazmus Sakib Ahmed <48786667+33sakib33@users.noreply.github.com>
2023-12-15 19:09:12 +0600

    first

---
+++ src/hotel/management/system/CheckOut.java
@@ -0,0 +1,131 @@
+package hotel.management.system;
+
+import java.awt.*;
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class CheckOut extends JFrame {
+	private JPanel contentPane;
+	private JTextField t1;
+	private Choice c1;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CheckOut frame = new CheckOut();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void close() {
+		this.dispose();
+	}
+
+	public CheckOut() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(530, 200, 800, 294);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		JLabel lblCheckOut = new JLabel("Check Out ");
+		lblCheckOut.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblCheckOut.setBounds(70, 11, 140, 35);
+		contentPane.add(lblCheckOut);
+
+		JLabel lblName = new JLabel("Number :");
+		lblName.setBounds(20, 85, 80, 14);
+		contentPane.add(lblName);
+
+		c1 = new Choice();
+		try {
+			// Read room numbers from the file
+			ArrayList<String> roomNumbers = readRoomNumbersFromFile("rooms.txt");
+			for (String roomNumber : roomNumbers) {
+				c1.add(roomNumber.split(",")[0]);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		c1.setBounds(130, 82, 150, 20);
+		contentPane.add(c1);
+
+		JButton l2 = new JButton();
+		l2.setBounds(290, 82, 20, 20);
+		add(l2);
+
+		l2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent ae) {
+				System.out.println("Hi");
+				try {
+					String number = c1.getSelectedItem();
+					t1.setText(number);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+
+		JLabel lblRoomNumber = new JLabel("Room Number:");
+		lblRoomNumber.setBounds(20, 132, 86, 20);
+		contentPane.add(lblRoomNumber);
+
+		t1 = new JTextField();
+		t1.setBounds(130, 132, 150, 20);
+		contentPane.add(t1);
+
+		JButton btnCheckOut = new JButton("Check Out");
+		btnCheckOut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String id = c1.getSelectedItem();
+				String s1 = t1.getText();
+
+				// You can perform checkout actions here without the database
+
+				JOptionPane.showMessageDialog(null, "Check Out Successful");
+				// Example: new Reception().setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnCheckOut.setBounds(50, 200, 100, 25);
+		btnCheckOut.setBackground(Color.BLACK);
+		btnCheckOut.setForeground(Color.WHITE);
+		contentPane.add(btnCheckOut);
+
+		JButton btnExit = new JButton("Back");
+		btnExit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				// Example: new Reception().setVisible(true);
+				setVisible(false);
+			}
+		});
+		btnExit.setBounds(160, 200, 100, 25);
+		btnExit.setBackground(Color.BLACK);
+		btnExit.setForeground(Color.WHITE);
+		contentPane.add(btnExit);
+
+		getContentPane().setBackground(Color.WHITE);
+	}
+
+	private ArrayList<String> readRoomNumbersFromFile(String fileName) throws IOException {
+		ArrayList<String> roomNumbers = new ArrayList<>();
+		try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				roomNumbers.add(line);
+			}
+		}
+		return roomNumbers;
+	}
+}
---
+++ src/hotel/management/system/Employee.java
@@ -0,0 +1,100 @@
+package hotel.management.system;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTable;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+//import net.proteanit.sql.DbUtils;
+
+public class Employee extends JFrame {
+	Connection conn = null;
+	private JPanel contentPane;
+	private JTable table;
+	private JLabel lblNewLabel;
+	private JLabel lblJob;
+	private JLabel lblName;
+	private JLabel lblDepartment;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Employee frame = new Employee();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public void close() {
+		this.dispose();
+	}
+
+	/**
+	 * Create the frame.
+	 *
+	 * @throws SQLException
+	 */
+	public Employee() throws SQLException {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(430, 200, 1000, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+
+		table = new JTable();
+		table.setBounds(0, 34, 1000, 450);
+		contentPane.add(table);
+
+
+		JButton btnLoadData = new JButton("Load Data");
+		btnLoadData.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					// Hardcoded data for testing
+					Object[][] data = {
+							{"1", "John Doe", "25", "Male", "IT"},
+							{"2", "Jane Smith", "30", "Female", "Finance"},
+							// Add more rows as needed
+					};
+
+					String[] columnNames = {"ID", "Name", "Age", "Gender", "Department"};
+
+					// Create a TableModel with hardcoded data
+					DefaultTableModel model = new DefaultTableModel(data, columnNames);
+
+					// Set the table model
+					table.setModel(model);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+			}
+		});
+		btnLoadData.setBounds(350, 500, 120, 30);
+		btnLoadData.setBackground(Color.BLACK);
+		btnLoadData.setForeground(Color.WHITE);
+		contentPane.add(btnLoadData);
+
+		// ... (rest of the code remains unchanged)
+
+		getContentPane().setBackground(Color.WHITE);
+	}
+}
